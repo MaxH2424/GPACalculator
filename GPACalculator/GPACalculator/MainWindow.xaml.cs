d9f4d915ad1419f9954e1752de515045c5f49e0e
Max Hendricks <go4811@wayne.edu>
2019-12-12 01:17:22 -0500

    First Commit - Added Prototype UI

---
+++ GPACalculator/GPACalculator/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GPACalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string name;
+        private string date;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //System.IO.File.WriteAllLines(@"C:\Users\Max Hendricks\Downloads\C#\GPADoc", name);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            name = this.NameBox.Text;
+            date = this.NameBox.Text;
+            
+        }
+    }
+}
---
+++ GPACalculator/GPACalculator/Student.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPACalculator
+{
+    public class Student
+    {
+        private double gradePoints;
+        private int creditHours;
+        private string studentName;
+        private int classes;
+        private double gpa;
+
+        Student()
+        {
+            gradePoints = 0;
+            creditHours = 0;
+            studentName = "";
+            classes = 0;
+            gpa = 0;
+        }
+
+        Student(int points, int ch, string sn, int cl, double g)
+        {
+            gradePoints = points;
+            creditHours = ch;
+            studentName = sn;
+            classes = cl;
+            gpa = g;
+        }
+
+        public void SetName(string n)
+        {
+            studentName = n;
+        }
+
+        public string GetName()
+        {
+            return studentName;
+        }
+
+        public double GetPoints()
+        {
+            return gradePoints;
+        }
+
+        public void SetPoints(double p)
+        {
+            gradePoints = p;
+        }
+
+        public double GetHours()
+        {
+            return creditHours;
+        }
+
+        public void SetHours(int h)
+        {
+            creditHours = h;
+        }
+
+        public void SetClasses(int c)
+        {
+            classes = c;
+        }
+
+        public int GetClasses()
+        {
+            return classes;
+        }
+
+        public double GetGpa()
+        {
+            return gpa;
+        }
+
+        public void SetGpa(double g)
+        {
+            gpa = g;
+        }
+
+        public double SolveGpa(double points, double ch)
+        {
+            return (points / ch);
+        }
+
+    }
+}
